1f5edacd4241f245ceb32782853e09ab1456456c
Huy Le <lehuydit@gmail.com>
2020-12-18 10:00:05 +0700

    chore: test

--- server.js
+++ server.js
@@ -3,33 +3,71 @@ var express = require('express'),
   app = express();
 var request = require('request');
 var cheerio = require('cheerio');
+var Crisp = require("node-crisp-api")
+let CrispClient = new Crisp();
+const WEBSITE_ID = "xxxxxxx";
+CrispClient.authenticate('xxxxx', 'xxxxxxx');
+
 
 app.use(express.static('adminchelsea'));
 
 
 
-app.get('/api/v1', function(req, res, next) {
+app.get('/api/v1', function async (req, res, next) {
+  // request('http://www.chelseafc.com.vn/bang-xep-hang-premier-league.html', function(error, response, html) {
+  //   if (!error && response.statusCode == 200) {
+  //     console.log("Ok fine");
+  //     res.writeHead(200, {'Content-Type': 'text/plain'});
 
-  request('http://www.chelseafc.com.vn/bang-xep-hang-premier-league.html', function(error, response, html) {
-    if (!error && response.statusCode == 200) {
-      console.log(html);
-      var $ = cheerio.load(html);
-      // res.send(html);
+  //   // Send back a response and end the connection
+  //   res.end('Hello World!\n');
+  //     var $ = cheerio.load(html);
+  //     // res.send(html);
 
-      // $('.dynamic_blocks')
+  //     // $('.dynamic_blocks')
       
-      // .each(function(i, element){
-      //   var a = $(this).prev();
-      //   console.log(a.text());
-      // });
+  //     // .each(function(i, element){
+  //     //   var a = $(this).prev();
+  //     //   console.log(a.text());
+  //     // });
 
-    } else {
-        console.log('error...');
-    }
-  });
+  //   } else {
+  //       console.log('error...');
+  //   }
+  // });
+  CrispClient.websitePeople.findByEmail(WEBSITE_ID, 'lehuydit@gmail.com')
+    .then((data) => {
+        console.info("Sent message.", data);
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        // Send back a response and end the connection
+        res.end('Hello World!\n');
+      })
+      .catch((error) => {
+        console.error("Failed sending message:", error);
+      });
+
 });
 
-
+app.get('/api/v1/events', function async (req, res, next) {
+  // CrispClient.websitePeople.addPeopleEvent(websiteId, peopleId, params)
+  CrispClient.websitePeople.addPeopleEvent(WEBSITE_ID, 'lehuydit@gmail.com', {
+    text: "LoginOk",
+    color: "blue",
+    data: {
+      tenant: 'marktenant',
+      platform: 'ios'
+    }
+  })
+  .then((data) => {
+      console.info("Add Events successfully.", data);
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      // Send back a response and end the connection
+      res.end('Hello World!\n');
+    })
+    .catch((error) => {
+      console.error("Failed sending message:", error);
+    });
+});
 
 app.get('/api/shows', function(req, res, next) {
   var query = Show.find();
@@ -52,13 +90,6 @@ app.get('/api/shows/:id', function(req, res, next) {
     res.send(show);
   });
 });
-
-
-
-
-
-
-
 
 
 
