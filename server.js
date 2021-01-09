30630dede04663c33aabdb68f84da14fc72a7caf
Huy Le <lehuydit@gmail.com>
2021-01-09 17:33:54 +0700

    chore: test crisp

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ var request = require('request');
 var cheerio = require('cheerio');
 var Crisp = require("node-crisp-api")
 let CrispClient = new Crisp();
-const WEBSITE_ID = "xxxxxxx";
-CrispClient.authenticate('xxxxx', 'xxxxxxx');
+const WEBSITE_ID = "xxxx";
+CrispClient.authenticate('xxx', 'xxx');
 
 
 app.use(express.static('adminchelsea'));
@@ -14,48 +14,59 @@ app.use(express.static('adminchelsea'));
 
 
 app.get('/api/v1', function async (req, res, next) {
-  // request('http://www.chelseafc.com.vn/bang-xep-hang-premier-league.html', function(error, response, html) {
-  //   if (!error && response.statusCode == 200) {
-  //     console.log("Ok fine");
-  //     res.writeHead(200, {'Content-Type': 'text/plain'});
+  request('http://www.chelseafc.com.vn/bang-xep-hang-premier-league.html', function(error, response, html) {
+    if (!error && response.statusCode == 200) {
+      console.log("Ok fine");
+      res.writeHead(200, {'Content-Type': 'text/plain'});
 
-  //   // Send back a response and end the connection
-  //   res.end('Hello World!\n');
-  //     var $ = cheerio.load(html);
-  //     // res.send(html);
+    // Send back a response and end the connection
+    res.end('Hello World!\n');
+      var $ = cheerio.load(html);
+      // res.send(html);
 
-  //     // $('.dynamic_blocks')
+      // $('.dynamic_blocks')
       
-  //     // .each(function(i, element){
-  //     //   var a = $(this).prev();
-  //     //   console.log(a.text());
-  //     // });
+      // .each(function(i, element){
+      //   var a = $(this).prev();
+      //   console.log(a.text());
+      // });
 
-  //   } else {
-  //       console.log('error...');
-  //   }
-  // });
-  CrispClient.websitePeople.findByEmail(WEBSITE_ID, 'lehuydit@gmail.com')
-    .then((data) => {
-        console.info("Sent message.", data);
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        // Send back a response and end the connection
-        res.end('Hello World!\n');
-      })
-      .catch((error) => {
-        console.error("Failed sending message:", error);
-      });
+    } else {
+        console.log('error...');
+    }
+  });
 
 });
 
-app.get('/api/v1/events', function async (req, res, next) {
-  // CrispClient.websitePeople.addPeopleEvent(websiteId, peopleId, params)
-  CrispClient.websitePeople.addPeopleEvent(WEBSITE_ID, 'lehuydit@gmail.com', {
-    text: "LoginOk",
+const registerPeople = (res) => {
+  CrispClient.websitePeople.createNewPeopleProfile(WEBSITE_ID, {
+    email: 'lehuydit1@gmail.com',
+    person: {
+      nickname: 'Huy test',
+      avatar: 'https://static.vecteezy.com/system/resources/thumbnails/000/135/345/small/new-year-icon-vector.jpg'
+    },
+    segments: ['dev', 'tenant:dev']
+  })
+  .then((data) => {
+      console.info("Add Events successfully.", data);
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      // Send back a response and end the connection
+      res.end('Registered new people!\n');
+    })
+    .catch((error) => {
+      console.error("Failed to register new people message:", error);
+      res.end('Error!\n');
+    });
+}
+
+const registerPeopleProfile = (res) => {
+  CrispClient.websitePeople.addPeopleEvent(WEBSITE_ID, 'lehuydit1@gmail.com', {
+    text: "Login-android",
     color: "blue",
     data: {
       tenant: 'marktenant',
-      platform: 'ios'
+      platform: 'android',
+      timestamp: new Date().valueOf()
     }
   })
   .then((data) => {
@@ -66,7 +77,22 @@ app.get('/api/v1/events', function async (req, res, next) {
     })
     .catch((error) => {
       console.error("Failed sending message:", error);
+      // res.end('Error!\n');
+      // Failed sending message: {
+      //   reason: 'error',
+      //   message: 'people_not_found',
+      //   code: 404,
+      //   data: {
+      //     namespace: 'response',
+      //     message: 'Got response error: people_not_found'
+      //   }
+      // }
+      registerPeople(res)
     });
+}
+
+app.get('/api/v1/events', function async (req, res, next) {
+  registerPeopleProfile(res)
 });
 
 app.get('/api/shows', function(req, res, next) {
